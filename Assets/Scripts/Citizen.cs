d524a50c3a20a2b609472c476b2c2c1c0014394d
PC-JORGE\Jorge <sebaspb0202@gmail.com>
2018-04-07 16:29:04 -0500

    Se completa el comentado del código y se corrigen algunos errores menores.

--- Assets/Scripts/Citizen.cs
+++ Assets/Scripts/Citizen.cs
@@ -4,15 +4,18 @@ using UnityEngine;
 
 public class Citizen : MonoBehaviour
 {
-
-    public CitizenInformation cInformation;
-
+    //we create a new structure that will store the citizen information (age and name).
     public struct CitizenInformation
     {
         public int age;
         public CitizenName name;
     }
 
+    //We create a new structure with the previous data called cInformation.
+    public CitizenInformation cInformation;
+
+
+    //We create a new enumerator that will store all the possible names for the citizen.
     public enum CitizenName
     {
       	Jesús,
@@ -35,20 +38,19 @@ public class Citizen : MonoBehaviour
         Augusto,
         Ned,
         David
-
-
     }
 
    
-
+    //Start is called only once.
     void  Start()
     {
-        int nombres = CitizenName.GetNames(typeof(CitizenName)).Length;
+        //In the age information of the structure we create a random number between 15 and 100;
         cInformation.age = Random.Range(15, 100);
+        //We create a new int that is equal to the number of elements in the names enumerator.
+        int nombres = CitizenName.GetNames(typeof(CitizenName)).Length;
+        //In the name information of the structure we store a random name of the list between 0 and the previous number position.
         cInformation.name = (CitizenName)Random.Range(0, nombres);
     }
-
-   
-    }
+ }
 
 
